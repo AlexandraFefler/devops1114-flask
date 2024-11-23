4484ce9a16c2c98ea88e121345851faab82668f5
AlexandraFefler <sasha.fefler@gmail.com>
2024-11-23 14:40:08 +0200

    Spent too much time picking a font

--- app.py
+++ app.py
@@ -10,15 +10,16 @@ app = Flask(__name__)
 # def index_custom_greeting():
 #     return render_template("index.html", name)
 
-#            chatgpt's:            #
 # Home Route
 @app.route("/")
-def home(): # triggered when accesing /, on localhost (127.0.0.1:5000/)
+def home(): # triggered when accesing /, on localhost port 5000 (127.0.0.1:5000/)
     # return "Hello, World!"
     return render_template("index.html", name = "World")
 
 # Greeting Route
 @app.route("/greet/<name>") # /greet is the function greet(), and /<name> is mentioning that everything that's written in /greet/ is the value of a variable called 'name'
+# greet() is triggered by accessing /greet, and it needs to get an argument - it gets it from accessing /greet/whatever_name
+# if you try to access only "/greet" or "/greet/", the page will show it's not found (404), so @app.route() defines the only route to trigger greet()
 def greet(name): # greet() gets it's name arg by trying to access it in the url of the localhost (127.0.0.1:5000/greet/<name>), <name> is where arg name gets its value
     # return f"Hello, {name}!"
     return render_template("index.html", name = name)
