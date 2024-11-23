f4daebef6198e6027daafae4631fad21c467b204
AlexandraFefler <sasha.fefler@gmail.com>
2024-11-23 16:06:16 +0200

    Added a web game from www.htmlgames.com

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route("/")
 def home(): # triggered when accesing /, on localhost port 5000 (127.0.0.1:5000/)
     # return "Hello, World!"
-    return render_template("index.html", name = "World")
+    return render_template("original_index.html", name = "World")
 
 # Greeting Route
 @app.route("/greet/<name>") # /greet is the function greet(), and /<name> is mentioning that everything that's written in /greet/ is the value of a variable called 'name'
@@ -22,7 +22,7 @@ def home(): # triggered when accesing /, on localhost port 5000 (127.0.0.1:5000/
 # if you try to access only "/greet" or "/greet/", the page will show it's not found (404), so @app.route() defines the only route to trigger greet()
 def greet(name): # greet() gets it's name arg by trying to access it in the url of the localhost (127.0.0.1:5000/greet/<name>), <name> is where arg name gets its value
     # return f"Hello, {name}!"
-    return render_template("index.html", name = name)
+    return render_template("original_index.html", name = name)
 
 if __name__ == "__main__":
     app.run(debug=True)
